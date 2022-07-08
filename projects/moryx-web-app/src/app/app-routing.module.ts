84ac1d79832c48897cf2bbed6c000ed15e6ccc95
Marisa Goergen <mgoergen@phoenixcontact.com>
2022-07-08 13:20:07 +0200

    Fix routing

--- projects/moryx-web-app/src/app/app-routing.module.ts
+++ projects/moryx-web-app/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { EntryEditorDemoComponent } from './entry-editor-demo/entry-editor-demo.component';
 
 const routes: Routes = [
-  {path: 'entry-editor-demo/:entryParams', component:EntryEditorDemoComponent},
-  {path: '', component:AppComponent}
+  {path: 'entry-editor-demo/:entryParams', component:EntryEditorDemoComponent}
 ];
 
 @NgModule({
