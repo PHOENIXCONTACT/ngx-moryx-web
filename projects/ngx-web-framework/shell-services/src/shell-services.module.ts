539e4d4ac14bd358a62826c6d9dee8c80bccb9c4
Milad Katebi <mkatebi@phoenixcontact.com>
2022-09-20 14:28:30 +0200

    SMAF-3293: Feature/Added language header inceptor

--- projects/ngx-web-framework/shell-services/src/shell-services.module.ts
+++ projects/ngx-web-framework/shell-services/src/shell-services.module.ts
@@ -1,17 +1,20 @@
-import { Injectable, NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { forwardRef, Injectable, NgModule, Provider } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ApiInterceptor } from './api-interceptor/api-interceptor.service';
 import { LanguageService } from './language-service/languageService.service';
 import { SearchBarService } from './search-bar/search-bar.service';
 
+export const API_INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useExisting: forwardRef(() => ApiInterceptor),
+  multi: true,
+};
+
 @NgModule({
-    declarations: [
-        SearchBarService,
-        LanguageService
-    ],
-    imports: [
-        Injectable,
-        Observable
-    ],
-    exports: [SearchBarService, LanguageService],
+  declarations: [SearchBarService, LanguageService],
+  imports: [Injectable, Observable],
+  exports: [SearchBarService, LanguageService],
+  providers: [ApiInterceptor, API_INTERCEPTOR_PROVIDER],
 })
-export class ShellServicesModule { }
+export class ShellServicesModule {}
