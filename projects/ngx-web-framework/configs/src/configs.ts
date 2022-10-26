97a64756198afd2dd312f4316a261afa9817c7f9
Milad Katebi <Q9IDFE@europe.phoenixcontact.com>
2022-10-26 17:35:05 +0200

    Added configs

---
+++ projects/ngx-web-framework/configs/src/configs.ts
@@ -0,0 +1,3 @@
+export interface Configs {
+  identityUrl: string;
+}
--- projects/ngx-web-framework/public-api.ts
+++ projects/ngx-web-framework/public-api.ts
@@ -2,10 +2,11 @@
  * Public API Surface of ngx-web-framework
  */
 
-export * from '@moryx/ngx-web-framework/entry-editor'
-export * from '@moryx/ngx-web-framework/interceptors'
+export * from '@moryx/ngx-web-framework/configs'
 export * from '@moryx/ngx-web-framework/empty-state'
-export * from '@moryx/ngx-web-framework/shell-services'
-export * from '@moryx/ngx-web-framework/moryx-snackbar'
+export * from '@moryx/ngx-web-framework/entry-editor'
 export * from '@moryx/ngx-web-framework/iam'
+export * from '@moryx/ngx-web-framework/interceptors'
+export * from '@moryx/ngx-web-framework/moryx-snackbar'
+export * from '@moryx/ngx-web-framework/shell-services'
 
