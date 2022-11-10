507ba490399874499fedced9010a07b6c2d0776b
Christian Siewert <csiewert@phoenixcontact.com>
2022-11-10 11:38:47 +0100

    Fix `if-has-permission` directive to show elements when IAM is not configured

--- projects/ngx-web-framework/iam/src/directives/moryx-if-has-permission.directive.spec.ts
+++ projects/ngx-web-framework/iam/src/directives/moryx-if-has-permission.directive.spec.ts
@@ -22,31 +22,34 @@ class TestComponent {
   ignore = true;
 }
 
+class AuthServiceStub {
+  rootUrl: string | undefined | null = 'https://localhost/';
+
+  getUserPermissions$Json(filter?: string, context?: HttpContext) {
+    return of(new Array<string>('Permissions.Permitted'));
+  }
+}
+
 describe('MoryxIfHasPermissionDirective', () => {
   let fixture: ComponentFixture<TestComponent> | null;
   let component: TestComponent;
 
-  let authServiceStub = {
-    getUserPermissions$Json: function (filter?: string, context?: HttpContext) {
-      return of(new Array<string>('Permissions.Permitted'));
-    },
-  };
+  let authServiceStub: AuthServiceStub | null = new AuthServiceStub();
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
-        imports: [],
-        declarations: [MoryxIfHasPermissionDirective, TestComponent],
-        providers: [{ provide: AuthService, useValue: authServiceStub }],
-      }).createComponent(TestComponent);
+      imports: [],
+      declarations: [MoryxIfHasPermissionDirective, TestComponent],
+      providers: [{ provide: AuthService, useValue: authServiceStub }],
+    }).createComponent(TestComponent);
     component = fixture.componentInstance;
+    component.ignore = false;
 
     fixture.detectChanges();
   });
 
   it('should show permitted elements', (done) => {
-    const element = getElement(fixture);
-    component.ignore = false;
-    fixture?.detectChanges();
+    const element = getElement(fixture); 
     fixture?.whenStable().then(() => {
       expect(element?.nativeElement.innerHTML).toContain('This is permitted');
       done();
@@ -55,15 +58,16 @@ describe('MoryxIfHasPermissionDirective', () => {
 
   it('should hide forbidden elements', (done) => {
     const element = getElement(fixture);
-    component.ignore = false;
-    fixture?.detectChanges();
     fixture?.whenStable().then(() => {
       expect(element?.nativeElement.innerHTML).not.toContain('This is forbidden');
       done();
     });
   });
 
-  it('should show forbidden elements when *excepted*', (done) => {
+  it('should show forbidden elements when *exccepted*', (done) => {
+    component.ignore = true;
+    fixture?.detectChanges();
+
     const element = getElement(fixture);
     fixture?.whenStable().then(() => {
       expect(element?.nativeElement.innerHTML).toContain('This is forbidden');
@@ -75,6 +79,71 @@ describe('MoryxIfHasPermissionDirective', () => {
     fixture = null;
   });
 });
+
+describe('MoryxIfHasPermissionDirective without configuration', () => {
+  let fixture: ComponentFixture<TestComponent> | null;
+  let component: TestComponent;
+
+  let authServiceStub: AuthServiceStub | null = new AuthServiceStub();
+
+  beforeEach(() => {
+    authServiceStub!.rootUrl = '';
+    fixture = TestBed.configureTestingModule({
+      imports: [],
+      declarations: [MoryxIfHasPermissionDirective, TestComponent],
+      providers: [{ provide: AuthService, useValue: authServiceStub }],
+    }).createComponent(TestComponent);
+    component = fixture.componentInstance;
+    component.ignore = false;
+   
+    fixture.detectChanges();
+  });
+
+  it('should show forbidden elements when root url is empty', (done) => {
+    const element = getElement(fixture);
+    fixture?.whenStable().then(() => {
+      expect(element?.nativeElement.innerHTML).toContain('This is permitted');
+      expect(element?.nativeElement.innerHTML).toContain('This is forbidden');
+      done();
+    });
+  });
+  
+  it('should show forbidden elements when root url is undefined', (done) => {
+    authServiceStub!.rootUrl = undefined; 
+    triggerChangeDetection(component, fixture);
+
+    const element = getElement(fixture);
+    fixture?.whenStable().then(() => {
+      expect(element?.nativeElement.innerHTML).toContain('This is permitted');
+      expect(element?.nativeElement.innerHTML).toContain('This is forbidden');
+      done();
+    });
+  });
+
+  it('should show forbidden elements when root url is null', (done) => {
+    authServiceStub!.rootUrl = null; 
+    triggerChangeDetection(component, fixture);
+
+    const element = getElement(fixture);
+    fixture?.whenStable().then(() => {
+      expect(element?.nativeElement.innerHTML).toContain('This is permitted');
+      expect(element?.nativeElement.innerHTML).toContain('This is forbidden');
+      done();
+    });
+  });
+
+  afterEach(() => {
+    fixture = null;
+  });
+});
+
+function triggerChangeDetection(component: TestComponent, fixture: ComponentFixture<TestComponent> | null) {
+  // actually change object properties to have change detection triggered
+  component.ignore = !component.ignore;
+  fixture?.detectChanges();
+  component.ignore = !component.ignore;
+  fixture?.detectChanges();
+}
 
 function getElement(fixture: ComponentFixture<any> | null): DebugElement | undefined {
   return fixture?.debugElement;
