b02c9bf3a3a2b091cbdab0567a410dd0e5e63882
Milad Katebi <Q9IDFE@europe.phoenixcontact.com>
2022-11-21 09:51:46 +0100

    updated to 15

--- projects/ngx-web-framework/entry-editor/src/entry-editor.module.ts
+++ projects/ngx-web-framework/entry-editor/src/entry-editor.module.ts
@@ -1,26 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
-import { MatIconModule} from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 
-import { EntryEditorComponent } from './entry-editor/entry-editor.component';
 import { BooleanEditorComponent } from './boolean-editor/boolean-editor.component';
-import { InputEditorComponent } from './input-editor/input-editor.component';
-import { EnumEditorComponent } from './enum-editor/enum-editor.component';
-import { NavigableEntryEditorComponent } from './navigable-entry-editor/navigable-entry-editor.component';
-import { EntryObjectComponent } from './entry-object/entry-object.component';
-import { FileEditorComponent } from './file-editor/file-editor.component';
+import { EntryEditorComponent } from './entry-editor/entry-editor.component';
 import { EntryListItemComponent } from './entry-list-item/entry-list-item.component';
-
-
+import { EntryObjectComponent } from './entry-object/entry-object.component';
+import { EnumEditorComponent } from './enum-editor/enum-editor.component';
+import { FileEditorComponent } from './file-editor/file-editor.component';
+import { InputEditorComponent } from './input-editor/input-editor.component';
+import { NavigableEntryEditorComponent } from './navigable-entry-editor/navigable-entry-editor.component';
 
 @NgModule({
   declarations: [
@@ -31,17 +29,21 @@ import { EntryListItemComponent } from './entry-list-item/entry-list-item.compon
     FileEditorComponent,
     NavigableEntryEditorComponent,
     EntryObjectComponent,
-    EntryListItemComponent
+    EntryListItemComponent,
   ],
   imports: [
     CommonModule,
-    FormsModule, ReactiveFormsModule,
-    MatListModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSelectModule, MatCardModule,
-    MatButtonModule, MatIconModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
-  exports: [
-    EntryEditorComponent,
-    NavigableEntryEditorComponent
-  ]
+  exports: [EntryEditorComponent, NavigableEntryEditorComponent],
 })
-export class EntryEditorModule { }
+export class EntryEditorModule {}
