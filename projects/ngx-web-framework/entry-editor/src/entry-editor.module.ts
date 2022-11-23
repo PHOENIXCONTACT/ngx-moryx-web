89e02a6123524846ad8c67b94a6597d6fcfa99e2
Milad Katebi <Q9IDFE@europe.phoenixcontact.com>
2022-11-23 12:58:41 +0100

    Revert "updated to 15"
    This reverts commit b02c9bf3a3a2b091cbdab0567a410dd0e5e63882.

--- projects/ngx-web-framework/entry-editor/src/entry-editor.module.ts
+++ projects/ngx-web-framework/entry-editor/src/entry-editor.module.ts
@@ -1,24 +1,26 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
 
-import { BooleanEditorComponent } from './boolean-editor/boolean-editor.component';
 import { EntryEditorComponent } from './entry-editor/entry-editor.component';
-import { EntryListItemComponent } from './entry-list-item/entry-list-item.component';
-import { EntryObjectComponent } from './entry-object/entry-object.component';
-import { EnumEditorComponent } from './enum-editor/enum-editor.component';
-import { FileEditorComponent } from './file-editor/file-editor.component';
+import { BooleanEditorComponent } from './boolean-editor/boolean-editor.component';
 import { InputEditorComponent } from './input-editor/input-editor.component';
+import { EnumEditorComponent } from './enum-editor/enum-editor.component';
 import { NavigableEntryEditorComponent } from './navigable-entry-editor/navigable-entry-editor.component';
+import { EntryObjectComponent } from './entry-object/entry-object.component';
+import { FileEditorComponent } from './file-editor/file-editor.component';
+import { EntryListItemComponent } from './entry-list-item/entry-list-item.component';
+
+
 
 @NgModule({
   declarations: [
@@ -29,21 +31,17 @@ import { NavigableEntryEditorComponent } from './navigable-entry-editor/navigabl
     FileEditorComponent,
     NavigableEntryEditorComponent,
     EntryObjectComponent,
-    EntryListItemComponent,
+    EntryListItemComponent
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatListModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatSelectModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
+    FormsModule, ReactiveFormsModule,
+    MatListModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatSelectModule, MatCardModule,
+    MatButtonModule, MatIconModule
   ],
-  exports: [EntryEditorComponent, NavigableEntryEditorComponent],
+  exports: [
+    EntryEditorComponent,
+    NavigableEntryEditorComponent
+  ]
 })
-export class EntryEditorModule {}
+export class EntryEditorModule { }
--- projects/ngx-web-framework/entry-editor/src/file-editor/file-editor.component.ts
+++ projects/ngx-web-framework/entry-editor/src/file-editor/file-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { UntypedFormControl, ValidatorFn, Validators } from '@angular/forms';
 import { Entry } from '../models/entry';
 import { EntryValueType } from '../models/entry-value-type';
+import { FormControl, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'entry-file-editor',
@@ -21,7 +21,7 @@ export class FileEditorComponent implements OnInit {
   }
   get disabled(): boolean { return this._disabled; }
 
-  inputFormControl!: UntypedFormControl; 
+  inputFormControl!: FormControl; 
 
   constructor() { }
 
@@ -30,11 +30,11 @@ export class FileEditorComponent implements OnInit {
     if (this.entry.validation?.isRequired)
       validators.push(Validators.required);
 
-    this.inputFormControl = new UntypedFormControl(
+    this.inputFormControl = new FormControl(
       {
         value: this.entry.description ?? '',
         disabled: this.disabled 
-          || (this.entry.value.isReadOnly ?? false) 
+          || (this.entry.value?.isReadOnly ?? false) 
           || this.entry.value?.type === EntryValueType.Stream
       }, 
       validators);
