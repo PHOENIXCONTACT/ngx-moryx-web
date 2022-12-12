de6d21f98c1a2e18f4c3a4669f9a45892c99f740
Matho Camara <mcamara@phoenixcontact.com>
2022-12-12 16:10:05 +0100

    SMAF-3885/SMAF-3884 : Possible values does not work for Product Attributes/Can not set a float value greater than 4

--- projects/ngx-web-framework/entry-editor/src/validators/entry-editor.validators.ts
+++ projects/ngx-web-framework/entry-editor/src/validators/entry-editor.validators.ts
@@ -1,23 +1,25 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { EntryValueType } from '../models/entry-value-type';
 
-export function invalidEntryValueValidator(entryType: EntryValueType |undefined): ValidatorFn {
+export function invalidEntryValueValidator(entryType: EntryValueType | undefined): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const invalid = isInvalid(control.value, entryType);
-    return invalid ? { invalidEntryValue:true } : null;
+    return invalid ? { invalidEntryValue: true } : null;
   };
 }
 
-export function isInvalid(value: any, entryType: EntryValueType|undefined): boolean {
-  //integer, Single or Double Types
+export function isInvalid(value: any, entryType: EntryValueType | undefined): boolean {
+  //integer, Single  Types
   if (
     entryType === EntryValueType.Int16 ||
     entryType === EntryValueType.Int32 ||
-    entryType === EntryValueType.Int64 ||
-    entryType === EntryValueType.Single ||
-    entryType === EntryValueType.Double 
+    entryType === EntryValueType.Int64
   ) {
     if (!Number.isInteger(Number(value))) return true;
+  }
+  //  Double types
+  else if (entryType === EntryValueType.Double || entryType === EntryValueType.Single) {
+    if (Number.isNaN(Number(value)) ) return true;
   }
   //Unsigned integers
   else if (
@@ -28,12 +30,9 @@ export function isInvalid(value: any, entryType: EntryValueType|undefined): bool
     if (!Number.isInteger(Number(value)) && !(Number(value) >= 0)) return true;
   }
   //boolean type
-  else if(entryType === EntryValueType.Boolean)
-  {
-    if(!(typeof value === 'boolean')) return true;
+  else if (entryType === EntryValueType.Boolean) {
+    if (!(typeof value === 'boolean')) return true;
   }
 
   return false;
 }
-
-
