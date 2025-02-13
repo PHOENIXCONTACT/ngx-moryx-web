aa6ef048c6dbf37df0cb7c809a8c9ce86a033398
Christian Siewert <csiewert@phoenixcontact.com>
2025-02-13 14:38:04 +0100

    Update `enum-editor.component` to use angular signals

--- projects/ngx-web-framework/entry-editor/src/enum-editor/enum-editor.component.ts
+++ projects/ngx-web-framework/entry-editor/src/enum-editor/enum-editor.component.ts
@@ -1,30 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, effect, input, model, } from '@angular/core';
 import { Entry } from '../models/entry';
-import { EntryValueType } from '../models/entry-value-type';
-import { PrototypeToEntryConverter } from '../prototype-to-entry-converter';
+import { CommonModule } from '@angular/common';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'entry-enum-editor',
     templateUrl: './enum-editor.component.html',
     styleUrls: ['./enum-editor.component.scss'],
-    standalone: false
+    standalone: true,
+    imports:[CommonModule, MatFormField, MatLabel, MatSelect, MatOption, FormsModule]
 })
-export class EnumEditorComponent implements OnInit {
-  private _entry!: Entry;
-  @Input() set entry(value: Entry) {
-    this._entry = value;
-  }
-  @Input() disabled: boolean = false;
-  disableSetEntryType: boolean = false;
+export class EnumEditorComponent {
+  disabled = input<boolean>(false);
+  entry = model.required<Entry>();
 
-  constructor() {}
-
-  get entry() {
-    return this._entry;
+  constructor() {
+    const reference = effect(() => {
+      this.initialize(this.entry());
+      reference.destroy();
+    });
   }
 
-  ngOnInit(): void {
-    this._entry.value.current = this._entry.value?.current ?? this._entry.value?.default;
+  initialize(entry: Entry) {
+    this.entry.update(e => {
+      let copy = Object.assign({}, e);
+      copy.value.current = entry.value?.current ?? entry.value?.default;
+      return copy;
+    });
   }
-
 }
