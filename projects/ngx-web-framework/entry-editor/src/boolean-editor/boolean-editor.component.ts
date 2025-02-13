c00813efa7ffa9bf52bbe8282475f408ce0b64b1
Christian Siewert <csiewert@phoenixcontact.com>
2025-02-13 14:36:26 +0100

    Update `boolean-editor.component` to use angular signals

--- projects/ngx-web-framework/entry-editor/src/boolean-editor/boolean-editor.component.ts
+++ projects/ngx-web-framework/entry-editor/src/boolean-editor/boolean-editor.component.ts
@@ -1,51 +1,61 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  effect,
+  input,
+  model,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Entry } from '../models/entry';
+import { MatHint } from '@angular/material/form-field';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
-    selector: 'entry-boolean-editor',
-    templateUrl: './boolean-editor.component.html',
-    styleUrls: ['./boolean-editor.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    standalone: false
+  selector: 'entry-boolean-editor',
+  templateUrl: './boolean-editor.component.html',
+  styleUrls: ['./boolean-editor.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [CommonModule, MatHint, MatCheckbox, FormsModule, NgClass],
 })
 export class BooleanEditorComponent {
-  private _entry!: Entry;
-  @Input() set entry(value: Entry) {
-    this._entry = value;
-    this._name = value.displayName ?? this._entry.identifier ?? ""
-    this._description = value.description
-    this._checked =
-      (this.entry.value?.current ?? this.entry.value?.default)?.localeCompare('true', undefined, {
-        sensitivity: 'base',
-      }) === 0;
-  }
-  get entry(): Entry {
-    return this._entry;
-  }
-  @Input() disabled: boolean = false;
+  checked = signal<boolean>(false);
+  name = signal<string>('');
+  description = signal<string>('');
 
-  _name!: string;
-  get name(): string {
-    return this._name;
-  }
-  
-  _description: string | null | undefined;
-  get description(): string | null | undefined {
-    return this._description;
+  disabled = input<boolean>(false);
+  entry = model.required<Entry>();
+
+  constructor() {
+    const reference = effect(() => {
+      this.initialize(this.entry());
+      reference.destroy();
+    });
   }
 
-  _checked!: boolean;
-  set checked(value: boolean) {
-    this._checked = value;
-    this._entry.value.current = value.toString();
-  }
-  get checked(): boolean {
-    return this._checked;
+  initialize(entry: Entry) {
+    const defaultChecked = (entry.value?.current ?? entry.value?.default)?.localeCompare('true', undefined, {
+      sensitivity: 'base',
+    }) === 0;
+    this.checked.set(defaultChecked);
+    this.description.set(entry.description ?? '')
+    this.name.set(this.entry().displayName ?? '')
   }
 
-  constructor() {}
+  checkedUpdated(value: boolean) {
+    this.checked.update(e => !e);
+    this.entry.update(e => {
+      let copy = Object.assign({}, e);
+      copy.value.current = this.checked() + '';
+      return copy;
+    });
+  }
 
   clickContainer(event: MouseEvent) {
-    if ((event.target as HTMLElement).nodeName == 'MAT-CHECKBOX' && !this.disabled) this.checked = !this.checked;
+    if (!this.disabled()) {
+      this.checkedUpdated(!this.checked());
+    }
   }
 }
