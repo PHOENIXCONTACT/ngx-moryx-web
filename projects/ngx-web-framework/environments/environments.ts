bde7a3b2665cb5b42af1240eea8c959ba3e14e0b
André Niggemann <aniggemann@phoenixcontact.com>
2026-02-17 16:21:53 +0100

    Add exception at the end to avoid compiler errors, because not all cases are handled.

--- projects/ngx-web-framework/environments/environments.ts
+++ projects/ngx-web-framework/environments/environments.ts
@@ -7,7 +7,9 @@
 export function getPathBase(modulePrefix: string) {
   
   let baseElement = document.querySelector('base');
-  let href = baseElement?.href; // routingPrefix/commandcenter
+  
+  // maybe suprising: href contains not only prefix/modulePrefix that's visible in the html but a complete uri
+  let href = baseElement?.href; 
   if(modulePrefix == null || modulePrefix == undefined || modulePrefix == "")
     return href;
 
@@ -15,6 +17,9 @@ export function getPathBase(modulePrefix: string) {
     modulePrefix = '/' + modulePrefix;
   }
   if(href?.endsWith(modulePrefix)) {
-    return href.substring(0, href.length - modulePrefix.length)  // routingPrefix
+    return href.substring(0, href.length - modulePrefix.length)
   }
+  // TODO: consider if we need to handle cases where the modulePrefix does not exist or is not the end of the string
+  // For modules designed to work with moryx, I don't see how that could happen and what the appropriate error handling should be.
+  throw Error("Not implemented");
 }
