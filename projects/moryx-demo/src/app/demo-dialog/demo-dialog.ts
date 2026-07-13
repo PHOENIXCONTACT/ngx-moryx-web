c7f4c032ef7ed83aa98842a39429b4441bbe5c6f
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2026-07-13 11:43:44 +0200

    Added sample in moryx-demo for dialog

---
+++ projects/moryx-demo/src/app/demo-dialog/demo-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-dialog',
+  imports: [],
+  templateUrl: './demo-dialog.html',
+  styleUrl: './demo-dialog.scss',
+})
+export class DemoDialog {
+
+}
--- projects/moryx-demo/src/app/overview/overview.ts
+++ projects/moryx-demo/src/app/overview/overview.ts
@@ -1,14 +1,20 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DemoDialog } from '../demo-dialog/demo-dialog';
 
 @Component({
     selector: 'app-overview',
     templateUrl: './overview.html',
     styleUrls: ['./overview.scss'],
     changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [RouterLink, MatButtonModule]
+    imports: [RouterLink, MatButtonModule, MatDialogModule]
 })
 export class Overview {
+  private dialog = inject(MatDialog);
 
+  protected openDialog(): void {
+    this.dialog.open(DemoDialog);
+  }
 }
